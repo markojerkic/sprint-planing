90bec6169255b640d2937b15b42b336b893f312d
Marko Jerkić <markojerkic266@gmail.com>
2025-03-01 12:32:52 +0100

    move a bit around

--- cmd/web/room/room.go
+++ cmd/web/room/room.go
@@ -4,7 +4,6 @@ import (
 	"github.com/a-h/templ"
 	"github.com/labstack/echo/v4"
 	"github.com/markojerkic/spring-planing/internal/database"
-	"github.com/markojerkic/spring-planing/internal/database/dbgen"
 )
 
 type RoomRouter struct {
@@ -21,47 +20,7 @@ func NewRoomRouter(db *database.Database, group *echo.Group) *RoomRouter {
 	e.GET("", echo.WrapHandler(templ.Handler(CreateRoom())))
 	e.GET("/:id", r.roomDetailsHandler)
 	e.POST("", r.createRoom)
-	e.POST("/ticket", r.createTicketHandler)
+	e.POST("/ticket", r.createRoom)
 
 	return r
-}
-
-func (r *RoomRouter) createRoom(ctx echo.Context) error {
-	user := ctx.Get("user").(dbgen.User)
-	name := ctx.FormValue("roomName")
-
-	tx, err := r.db.DB.BeginTx(ctx.Request().Context(), nil)
-	if err != nil {
-		ctx.Logger().Errorf("Error creating transaction: %v", err)
-		return ctx.String(500, "Error creating transaction")
-	}
-	defer tx.Rollback()
-
-	q := r.db.Queries.WithTx(tx)
-	room, err := q.CreateRoom(ctx.Request().Context(), dbgen.CreateRoomParams{
-		Name:      name,
-		CreatedBy: user.ID,
-	})
-	if err != nil {
-		tx.Rollback()
-		ctx.Logger().Errorf("Error creating room: %v", err)
-		return ctx.String(500, "Error creating room")
-	}
-
-	err = q.AddUserToRoom(ctx.Request().Context(), dbgen.AddUserToRoomParams{
-		RoomID: room.ID,
-		UserID: user.ID,
-	})
-	if err != nil {
-		tx.Rollback()
-		ctx.Logger().Errorf("Error adding user to room: %v", err)
-		return ctx.String(500, "Error adding user to room")
-	}
-
-	if err := tx.Commit(); err != nil {
-		ctx.Logger().Errorf("Error committing transaction: %v", err)
-		return ctx.String(500, "Error committing transaction")
-	}
-	// return ctx.Redirect(302, fmt.Sprintf("/room/%d", room.ID))
-	return ctx.Redirect(302, "/")
 }
