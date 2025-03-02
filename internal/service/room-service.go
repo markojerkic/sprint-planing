94e2c75c3db5a7c26b74028cb861074e395c4374
Marko Jerkić <markojerkic266@gmail.com>
2025-03-02 19:23:08 +0100

    create ticket form

--- internal/service/room-service.go
+++ internal/service/room-service.go
@@ -50,12 +50,42 @@ func (r *RoomService) CreateRoom(ctx context.Context, userID int64, roomName str
 	return &room, nil
 }
 
-func (r *RoomService) GetRoom(ctx context.Context, roomID int64, userID int64) (dbgen.GetRoomDetailsRow, error) {
-	return r.db.Queries.GetRoomDetails(ctx, dbgen.GetRoomDetailsParams{
+func (r *RoomService) GetRoom(ctx context.Context, roomID int64, userID int64) (*dbgen.GetRoomDetailsRow, error) {
+	tx, err := r.db.DB.BeginTx(ctx, nil)
+	if err != nil {
+		slog.Error("Error creating transaction", slog.Any("error", err))
+		return nil, err
+	}
+	defer tx.Rollback()
+	q := r.db.Queries.WithTx(tx)
+
+	// Add user to room
+	err = q.AddUserToRoom(ctx, dbgen.AddUserToRoomParams{
+		RoomID: roomID,
+		UserID: userID,
+	})
+	if err != nil {
+		tx.Rollback()
+		slog.Error("Error adding user to room", slog.Any("error", err))
+		return nil, err
+	}
+
+	detail, err := q.GetRoomDetails(ctx, dbgen.GetRoomDetailsParams{
 		UserID: userID,
 		ID:     roomID,
 	})
 
+	if err != nil {
+		tx.Rollback()
+		slog.Error("Error getting room details", slog.Any("error", err))
+		return nil, err
+	}
+
+	if err := tx.Commit(); err != nil {
+		return nil, err
+	}
+
+	return &detail, err
 }
 
 func NewRoomService(db *database.Database) *RoomService {
