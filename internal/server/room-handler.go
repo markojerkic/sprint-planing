8ece41e1dba05b4294ef3e4f4f41613a651a8413
Marko Jerkić <markojerkic266@gmail.com>
2025-03-02 19:17:24 +0100

    create room

--- internal/server/room-handler.go
+++ internal/server/room-handler.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/a-h/templ"
 	"github.com/labstack/echo/v4"
@@ -20,13 +21,35 @@ func (r *RoomRouter) createRoomHandler(ctx echo.Context) error {
 	user := ctx.Get("user").(dbgen.User)
 	name := ctx.FormValue("roomName")
 
-	_, err := r.roomService.CreateRoom(ctx.Request().Context(), user.ID, name)
+	createdRoom, err := r.roomService.CreateRoom(ctx.Request().Context(), user.ID, name)
 	if err != nil {
 		ctx.Logger().Errorf("Error creating room: %v", err)
 		return ctx.String(500, "Error creating room")
 	}
 
-	return ctx.Redirect(302, fmt.Sprintf("/room/%s", name))
+	return ctx.Redirect(302, fmt.Sprintf("/room/%d", createdRoom.ID))
+}
+
+func (r *RoomRouter) roomDetailsHandler(ctx echo.Context) error {
+	user := ctx.Get("user").(dbgen.User)
+	roomID, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
+	if err != nil {
+		ctx.Logger().Errorf("Error parsing room id: %v", err)
+		return ctx.String(400, "Invalid room id")
+	}
+
+	roomDetails, err := r.roomService.GetRoom(ctx.Request().Context(), roomID, user.ID)
+	if err != nil {
+		ctx.Logger().Errorf("Error getting room: %v", err)
+		return ctx.String(500, "Error getting room")
+	}
+
+	return room.RoomPage(room.RoomPageProps{
+		ID:                 roomDetails.ID,
+		Name:               roomDetails.Name,
+		CreatedAt:          roomDetails.CreatedAt.Time,
+		IsCurrentUserOwner: roomDetails.IsOwner,
+	}).Render(ctx.Request().Context(), ctx.Response().Writer)
 }
 
 func newRoomRouter(db *database.Database, group *echo.Group) *RoomRouter {
@@ -36,6 +59,8 @@ func newRoomRouter(db *database.Database, group *echo.Group) *RoomRouter {
 	}
 	e := r.group
 	e.GET("", echo.WrapHandler(templ.Handler(room.CreateRoom())))
+	e.POST("", r.createRoomHandler)
+	e.GET("/:id", r.roomDetailsHandler)
 
 	return r
 }
