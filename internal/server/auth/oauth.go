45443cdf0c853bba73eed7579cbe18cf5e6ead81
Marko Jerkić <markojerkic266@gmail.com>
2025-03-23 17:45:36 +0100

    jira integration checkpoint can't save session

--- internal/server/auth/oauth.go
+++ internal/server/auth/oauth.go
@@ -1,13 +1,21 @@
 package auth
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	"log/slog"
+	"net/http"
 	"os"
 	"time"
 
+	"github.com/google/uuid"
+	"github.com/labstack/echo-contrib/session"
 	"github.com/labstack/echo/v4"
 	"golang.org/x/oauth2"
 )
+
+const sessionName = "user_session"
 
 type OAuthRouter struct {
 	BaseURL string
@@ -15,18 +23,84 @@ type OAuthRouter struct {
 }
 
 func (o *OAuthRouter) Login(c echo.Context) error {
-	state := time.Now().String()
+	state := uuid.New().String()
 	authUrl := o.Config.AuthCodeURL(state)
+
+	cookie := new(http.Cookie)
+	cookie.Name = "oauth_state"
+	cookie.Value = state
+	cookie.Expires = time.Now().Add(5 * time.Minute)
+	c.SetCookie(cookie)
 
 	return c.Redirect(302, authUrl)
 }
 
 func (o *OAuthRouter) Callback(c echo.Context) error {
-	slog.Info("Callback",
-		slog.Any("query", c.QueryParams()),
-		slog.Any("header", c.Request().Header))
+	// Verify state parameter
+	state := c.QueryParam("state")
+	cookie, err := c.Cookie("oauth_state")
+	if err != nil || cookie.Value != state {
+		slog.Error("Invalid state parameter", slog.Any("error", err), slog.String("cookie", cookie.Value), slog.String("state", state))
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid state parameter"})
+	}
+
+	// Exchange authorization code for token
+	code := c.QueryParam("code")
+	if code == "" {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Missing authorization code"})
+	}
+
+	token, err := o.ExchangeCodeForToken(c.Request().Context(), code)
+	if err != nil {
+		slog.Error("Failed to exchange code for token", slog.Any("error", err), slog.String("code", code))
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to exchange code for token"})
+	}
+
+	// Save token to session
+	session, err := session.Get(sessionName, c)
+	if err != nil {
+		return c.JSON(http.StatusUnauthorized, map[string]string{"error": "Failed to get session"})
+	}
+	session.Values["token"] = token
+	if err := session.Save(c.Request(), c.Response()); err != nil {
+		slog.Error("Failed to save session", slog.Any("error", err))
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to save session"})
+	}
 
 	return c.String(200, "Callback")
+}
+
+func (o *OAuthRouter) GetMyJiraIssues(c echo.Context) error {
+	session, err := session.Get(sessionName, c)
+	if err != nil {
+		return c.JSON(http.StatusUnauthorized, map[string]string{"error": "Failed to get session"})
+	}
+	token := session.Values["token"].(*oauth2.Token)
+
+	client := o.Config.Client(c.Request().Context(), token)
+	url := fmt.Sprintf("%s/rest/api/3/search?jql=%s", o.BaseURL, "assignee=currentUser()")
+	resp, err := client.Get(url)
+	if err != nil {
+		slog.Error("Failed to get issues", slog.Any("error", err))
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to get issues"})
+	}
+	defer resp.Body.Close()
+	var issues map[string]any
+	if err := json.NewDecoder(resp.Body).Decode(&issues); err != nil {
+		slog.Error("Failed to decode issues", slog.Any("error", err))
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to decode issues"})
+	}
+
+	return c.JSON(http.StatusOK, issues)
+}
+
+// ExchangeCodeForToken exchanges an authorization code for an access token
+func (c *OAuthRouter) ExchangeCodeForToken(ctx context.Context, code string) (*oauth2.Token, error) {
+	token, err := c.Config.Exchange(ctx, code)
+	if err != nil {
+		return nil, fmt.Errorf("failed to exchange code for token: %v", err)
+	}
+	return token, nil
 }
 
 func NewOAuthRouter(group *echo.Group) *OAuthRouter {
