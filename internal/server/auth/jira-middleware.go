cce043c4f914d352b13fb43a3aef18bca94e3720
Marko Jerkić <markojerkic266@gmail.com>
2025-03-29 14:21:38 +0100

    jira auth protection

--- internal/server/auth/jira-middleware.go
+++ internal/server/auth/jira-middleware.go
@@ -1,15 +1,17 @@
 package auth
 
 import (
-	"net/http"
+	"log/slog"
 
 	"github.com/labstack/echo/v4"
+	"github.com/markojerkic/spring-planing/cmd/web/components"
 )
 
 func JiraAuthMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		if _, ok := c.Get(JiraClientInfoKey).(*JiraClientInfo); !ok {
-			return c.JSON(http.StatusForbidden, "Jira client info not found")
+			slog.Warn("User not logged in via Jira, showing login page")
+			return components.LoginToJira().Render(c.Request().Context(), c.Response().Writer)
 		}
 
 		return next(c)
