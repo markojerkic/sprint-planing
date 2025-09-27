b486d1774476b28cd89f405bec3c9185120653a9
Marko Jerkić <markojerkic266@gmail.com>
2025-09-27 16:23:20 +0200

    check touched

--- cmd/web/assets/js/ws-reconnect.js
+++ cmd/web/assets/js/ws-reconnect.js
@@ -1,22 +1,29 @@
 let wsHasConnectedBefore = false;
 let isRefetching = false;
 
-htmx.on("htmx:wsOpen", function() {
+htmx.on("htmx:wsOpen", async function() {
     if (!wsHasConnectedBefore) {
         wsHasConnectedBefore = true;
         return;
     }
-
     if (isRefetching) {
         return;
+    }
+
+    while (hasFocusedInput()) {
+        console.log('Waiting for input to be unfocused...');
+        await new Promise(resolve => setTimeout(resolve, 3_000));
     }
 
     console.log('Reconnecting...');
     htmx.ajax('get', window.location.pathname, {
         target: '#ticket-list',
         select: '#ticket-list',
-        // swap: 'outerHTML'
     }).then(function() {
         isRefetching = false;
     });
 })
+
+function hasFocusedInput() {
+    return document.activeElement.tagName === 'INPUT';
+}
